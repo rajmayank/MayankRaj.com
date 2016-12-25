8b9a671807a278119c5916c218a2f0240d3fc3b5
Mayank <mayank9856@gmail.com>
2016-12-26 04:47:20 +0530

    Base setup

---
+++ Gruntfile.js
@@ -0,0 +1,83 @@
+module.exports = function(grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+			},
+			build: {
+				src: 'src/js/*.js',
+				dest: 'dist/js/script.min.js'
+			}
+		},
+		cssmin: {
+			options: {
+				shorthandCompacting: false,
+				roundingPrecision: -1
+			},
+			target: {
+				files: {
+					'dist/css/style.min.css': ['src/css/*.css']
+				}
+			}
+		},
+		htmlmin: { // Task 
+			dist: { // Target 
+				options: { // Target options 
+					removeComments: true,
+					collapseWhitespace: true
+				},
+				files: { // Dictionary of files 
+					'dist/index.html': 'src/index.html'
+				}
+			}
+		},
+		copy: {
+			main: {
+				files: [{
+					expand: true,
+					cwd: 'src/images/',
+					src: ['**/*.{png,jpg,svg}'],
+					dest: 'dist/images/'
+				}],
+			},
+		},
+		watch: {
+			scripts: {
+				files: ['src/js/*.js'],
+				tasks: ['uglify'],
+			},
+			css: {
+				files: ['src/css/*.css'],
+				tasks: ['cssmin']
+			},
+			html: {
+				files: ['src/*.html'],
+				tasks: ['htmlmin']
+			},
+			images: {
+				files: ['src/images/**'],
+				tasks: ['copy']
+			},
+			options: {
+				livereload: true
+			},
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-htmlmin');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+
+
+	// grunt.registerTask('default', ['uglify', 'cssmin', 'htmlmin', 'copy']);
+	// grunt.registerTask('copy', ['uglify', 'cssmin', 'htmlmin', 'copy']);
+	grunt.registerTask('default', ['copy' , 'watch']);
+
+};
