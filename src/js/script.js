6449fdc8c586192b3edd55577362a94e88294ef8
Mayank <mayank9856@gmail.com>
2017-01-01 03:54:01 +0530

    content update

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,8 @@
-(function () {
-    console.log('width');
-    $('.bar').each(function() {
-        var width = $(this).attr('data-progress');
-        console.log(width);
-        $(this).attr('style', 'width : ' + width + '%');
-    });
+(function() {
+	console.log('width');
+	$('.bar').each(function() {
+		var width = $(this).attr('data-progress');
+		console.log(width);
+		$(this).attr('style', 'width : ' + width + '%');
+	});
 })();
-
