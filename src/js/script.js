022069159360a91ce8b5e2f986be3308789ba57b
Mayank <mayank9856@gmail.com>
2017-01-01 19:47:37 +0530

    Content and style update

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,6 @@
 (function() {
-	console.log('width');
 	$('.bar').each(function() {
 		var width = $(this).attr('data-progress');
-		console.log(width);
 		$(this).attr('style', 'width : ' + width + '%');
 	});
 })();
