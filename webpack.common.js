f5405692e3dece3eb172ec7ccf5549ce14886f07
rajmayank <mayank9856@gmail.com>
2019-01-07 01:30:34 +0530

    asset minification & other minor updates

--- webpack.common.js
+++ webpack.common.js
@@ -51,12 +51,12 @@ module.exports = {
       prettyPrint: true
     }),
 
-    new CopyWebpackPlugin([
-      {
-        from: "./src/fonts/",
-        to: "fonts/",
-        flatten: true
-      }
-    ])
+    // new CopyWebpackPlugin([
+    //   {
+    //     from: "./src/fonts/",
+    //     to: "fonts/",
+    //     flatten: true
+    //   }
+    // ])
   ]
 };
