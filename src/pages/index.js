712628ca38c6a415af0f0650a9c82718f6b57f57
rajmayank <mayank9856@gmail.com>
2022-09-22 19:53:47 +0530

    Fixed link spacing issue

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,12 @@
 import React from "react";
-import {graphql, Link} from "gatsby";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faAws, faDocker, faJsSquare, faPython} from "@fortawesome/free-brands-svg-icons";
-
+import { graphql, Link } from "gatsby";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faAws,
+  faDocker,
+  faJsSquare,
+  faPython,
+} from "@fortawesome/free-brands-svg-icons";
 
 import BlogListing from "../components/blog-post-listing";
 
@@ -13,15 +17,13 @@ import "../styles/main.scss";
 import ImgBadgeSAA from "../assets/images/badges/aws-certified-solutions-architect-associate.png";
 import ImgBadgeSAP from "../assets/images/badges/aws-certified-solutions-architect-professional.png";
 import Seo from "../components/seo";
-import {OutboundLink} from "gatsby-plugin-google-analytics";
-import {faVideo} from "@fortawesome/free-solid-svg-icons";
-
+import { OutboundLink } from "gatsby-plugin-google-analytics";
+import { faVideo } from "@fortawesome/free-solid-svg-icons";
 
 const IndexHeader = () => {
   return (
     <header>
-
-      <Seo title="Home"/>
+      <Seo title="Home" />
 
       <div className="design-container">
         <div className="layer-1"></div>
@@ -32,25 +34,57 @@ const IndexHeader = () => {
       <div className="content-container">
         <div className="name-container">
           <div className="name-block">
-            <svg viewBox="0 0 140 80" xmlns='http://www.w3.org/2000/svg' version='1.1'>
-              <text text-anchor="start" x="10" y="30" className="text text-stroke" clip-path="url(#text1)"> Mayank
-              </text>
-              <text text-anchor="start" x="10" y="70" className="text text-stroke" clip-path="url(#text2)"> Raj
-              </text>
-              <text text-anchor="start" x="10" y="30" className="text text-stroke text-stroke-2"
-                    clip-path="url(#text1)">
+            <svg
+              viewBox="0 0 140 80"
+              xmlns="http://www.w3.org/2000/svg"
+              version="1.1"
+            >
+              <text
+                text-anchor="start"
+                x="10"
+                y="30"
+                className="text text-stroke"
+                clip-path="url(#text1)"
+              >
                 Mayank
               </text>
-              <text text-anchor="start" x="10" y="70" className="text text-stroke text-stroke-2"
-                    clip-path="url(#text2)">
+              <text
+                text-anchor="start"
+                x="10"
+                y="70"
+                className="text text-stroke"
+                clip-path="url(#text2)"
+              >
+                Raj
+              </text>
+              <text
+                text-anchor="start"
+                x="10"
+                y="30"
+                className="text text-stroke text-stroke-2"
+                clip-path="url(#text1)"
+              >
+                Mayank
+              </text>
+              <text
+                text-anchor="start"
+                x="10"
+                y="70"
+                className="text text-stroke text-stroke-2"
+                clip-path="url(#text2)"
+              >
                 Raj
               </text>
               <defs>
                 <clipPath id="text1">
-                  <text text-anchor="start" x="10" y="30" className="text"> Mayank</text>
+                  <text text-anchor="start" x="10" y="30" className="text">
+                    Mayank
+                  </text>
                 </clipPath>
                 <clipPath id="text2">
-                  <text text-anchor="start" x="10" y="70" className="text"> Raj</text>
+                  <text text-anchor="start" x="10" y="70" className="text">
+                    Raj
+                  </text>
                 </clipPath>
               </defs>
             </svg>
@@ -59,25 +93,34 @@ const IndexHeader = () => {
         <div className="description-container">
           <div className="row">
             <div className="description-block">
-              <span className="row-1 title animate">SaaS Founder & Associate Director of Engineering</span>
+              <span className="row-1 title animate">
+                SaaS Founder & Associate Director of Engineering
+              </span>
             </div>
           </div>
           <div className="badges">
-
-            <OutboundLink href="https://www.credly.com/badges/3b0f1aaa-7afd-4fb5-bb83-a1601f642bb2/public_url"
-                          target="_blank" rel="noreferrer">
-              <img src={ImgBadgeSAA}
-                   alt="AWS Certified Solutions Architect – Professional"
-                   className="animate"/>
-
+            <OutboundLink
+              href="https://www.credly.com/badges/3b0f1aaa-7afd-4fb5-bb83-a1601f642bb2/public_url"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src={ImgBadgeSAA}
+                alt="AWS Certified Solutions Architect – Professional"
+                className="animate"
+              />
             </OutboundLink>
-            <OutboundLink href="https://www.credly.com/badges/8a486510-a537-48f1-a29e-2643aa626be0/public_url"
-                          target="_blank" rel="noreferrer">
-              <img src={ImgBadgeSAP}
-                   alt="AWS Certified Solutions Architect – Professional"
-                   className="animate professional"/>
+            <OutboundLink
+              href="https://www.credly.com/badges/8a486510-a537-48f1-a29e-2643aa626be0/public_url"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src={ImgBadgeSAP}
+                alt="AWS Certified Solutions Architect – Professional"
+                className="animate professional"
+              />
             </OutboundLink>
-
           </div>
         </div>
       </div>
@@ -85,79 +128,121 @@ const IndexHeader = () => {
   );
 };
 
-const IndexBody = ({posts}) => {
+const IndexBody = ({ posts }) => {
   return (
     <div className="body-container" /*style="--mood: #8E8EC5"*/>
       <div className="content-container">
         <div className="text text-justify text-spacers">
-
-
           {/*TODO: Update this to track it*/}
 
           <div className="content-resume">
             <div className="icon">
-              <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
-                <path fill="currentColor"
-                      d="M369.9 97.98L286.02 14.1c-9-9-21.2-14.1-33.89-14.1H47.99C21.5.1 0 21.6 0 48.09v415.92C0 490.5 21.5 512 47.99 512h288.02c26.49 0 47.99-21.5 47.99-47.99V131.97c0-12.69-5.1-24.99-14.1-33.99zM256.03 32.59c2.8.7 5.3 2.1 7.4 4.2l83.88 83.88c2.1 2.1 3.5 4.6 4.2 7.4h-95.48V32.59zM272 480.01H112v-31.15c0-18.38 13.05-32.86 33.25-32.86 12.45 0 20.8 6.99 46.75 6.99 25.5 0 34.56-6.99 46.75-6.99 20.16 0 33.25 14.43 33.25 32.86v31.15zm80.01-16c0 8.8-7.2 16-16 16H304v-31.15c0-13.88-4.21-26.77-11.41-37.48-12.08-17.94-32.67-27.38-53.84-27.38-19.46 0-24.36 6.99-46.75 6.99-22.41 0-27.26-6.99-46.75-6.99-21.17 0-41.76 9.44-53.83 27.38-7.21 10.7-11.42 23.6-11.42 37.48v31.15H47.99c-8.8 0-16-7.2-16-16V48.09c0-8.8 7.2-16.09 16-16.09h176.04v104.07c0 13.3 10.7 23.93 24 23.93h103.98v304.01zM112 288c0 44.18 35.82 80 80 80s80-35.82 80-80c0-44.19-35.82-80-80-80s-80 35.81-80 80zm128 0c0 26.47-21.53 48-48 48s-48-21.53-48-48 21.53-48 48-48 48 21.53 48 48z">
-
-                </path>
+              <svg
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 384 512"
+              >
+                <path
+                  fill="currentColor"
+                  d="M369.9 97.98L286.02 14.1c-9-9-21.2-14.1-33.89-14.1H47.99C21.5.1 0 21.6 0 48.09v415.92C0 490.5 21.5 512 47.99 512h288.02c26.49 0 47.99-21.5 47.99-47.99V131.97c0-12.69-5.1-24.99-14.1-33.99zM256.03 32.59c2.8.7 5.3 2.1 7.4 4.2l83.88 83.88c2.1 2.1 3.5 4.6 4.2 7.4h-95.48V32.59zM272 480.01H112v-31.15c0-18.38 13.05-32.86 33.25-32.86 12.45 0 20.8 6.99 46.75 6.99 25.5 0 34.56-6.99 46.75-6.99 20.16 0 33.25 14.43 33.25 32.86v31.15zm80.01-16c0 8.8-7.2 16-16 16H304v-31.15c0-13.88-4.21-26.77-11.41-37.48-12.08-17.94-32.67-27.38-53.84-27.38-19.46 0-24.36 6.99-46.75 6.99-22.41 0-27.26-6.99-46.75-6.99-21.17 0-41.76 9.44-53.83 27.38-7.21 10.7-11.42 23.6-11.42 37.48v31.15H47.99c-8.8 0-16-7.2-16-16V48.09c0-8.8 7.2-16.09 16-16.09h176.04v104.07c0 13.3 10.7 23.93 24 23.93h103.98v304.01zM112 288c0 44.18 35.82 80 80 80s80-35.82 80-80c0-44.19-35.82-80-80-80s-80 35.81-80 80zm128 0c0 26.47-21.53 48-48 48s-48-21.53-48-48 21.53-48 48-48 48 21.53 48 48z"
+                ></path>
               </svg>
             </div>
-            <Link to="/Mayank_Raj_Resume.pdf" target="_blank" rel="noreferrer"> <span>Resume</span> </Link>
+            <Link to="/Mayank_Raj_Resume.pdf" target="_blank" rel="noreferrer">
+              <span>Resume</span>
+            </Link>
           </div>
 
-
           <div className="section-top-margin">
+            <p>
+              Hi, I'm Mayank Raj and I'm Solutions Architect. I have designed
+              and built systems for all the three major cloud providers - AWS,
+              Azure and GCP. While I primarily work in the domain of machine
+              learning/deep learning infrastructure, Big Data and Web APIs. I
+              have also dabbled with AR/VR, Voice etc.
+            </p>
 
-            <p>Hi, I'm Mayank Raj and I'm Solutions Architect. I have designed and built systems for all the three major
-              cloud providers - AWS, Azure and GCP. While I primarily work in the domain of machine learning/deep
-              learning infrastructure, Big Data and Web APIs. I have also dabbled with AR/VR, Voice etc. </p>
+            <p>
+              My primary focus is in building high performant products powered
+              by AI/ML & Big Data. I have designed systems that work at scale
+              and are yet cost effective to maintain, both in terms of humans
+              and machines. I have designed orchestration systems that manage
+              more than 250Tb of data. I have also designed serverless systems
+              that scale to handle ~100K long-running jobs while maintaining
+              sub-millisecond latency at the interfaces. When existing tools
+              fail, I have designed modules from scratch - I have built a graph
+              database engine to work with more than billions of nodes and
+              edges.
+            </p>
 
-            <p>My primary focus is in building high performant products powered by AI/ML & Big Data. I have designed
-              systems that work at scale and are yet cost effective to maintain, both in terms of humans and machines. I
-              have designed orchestration systems that manage more than 250Tb of data. I have also designed serverless
-              systems that scale to handle ~100K long-running jobs while maintaining sub-millisecond latency at the
-              interfaces. When existing tools fail, I have designed modules from scratch - I have built a graph database
-              engine to work with more than billions of nodes and edges.</p>
-
-            <p> I currently work with the amazing team at
-              <OutboundLink href="https://www.cactusglobal.com/">Cactus Communications</OutboundLink>. I've previously
-              worked with industry leaders like
-              <OutboundLink href="https://www.directi.com/">Directi</OutboundLink> while still in the second year of my
-              undergraduate study. Before that I've been involved with
+            <p>
+              I currently work with the amazing team at
+              <OutboundLink href="https://www.cactusglobal.com/">
+                Cactus Communications
+              </OutboundLink>
+              . I've previously worked with industry leaders like
+              <OutboundLink href="https://www.directi.com/">
+                Directi
+              </OutboundLink>
+              , while still in the second year of my undergraduate study. Before
+              that I've been involved with
               <OutboundLink href="https://frapp.in/">Frapp</OutboundLink>,
-              <OutboundLink href="http://workamp.co/">WorkAmp</OutboundLink> etc at various stages. I've also served as
-              the CTO for <OutboundLink href="https://aiesec.org/">AIESEC</OutboundLink> in Mumbai. </p>
+              <OutboundLink href="http://workamp.co/">WorkAmp</OutboundLink> etc
+              at various stages. I've also served as the CTO for
+              <OutboundLink href="https://aiesec.org/">AIESEC</OutboundLink> in
+              Mumbai.
+            </p>
 
-            <p>Having had started as a web designer & developer, I have helped startups set up their online presence.
-              From designing the web portals to developing, hosting and maintaining them. These initial few years
-              exposed me to JavaScript and the amazing community around it. The language still manages to throw new
-              things at me after all these years. (shoutout to Kyle Simpson for the
-              <OutboundLink href="https://github.com/getify/You-Dont-Know-JS">YDKJS</OutboundLink> series)</p>
+            <p>
+              Having had started as a web designer & developer, I have helped
+              startups set up their online presence. From designing the web
+              portals to developing, hosting and maintaining them. These initial
+              few years exposed me to JavaScript and the amazing community
+              around it. The language still manages to throw new things at me
+              after all these years. (shoutout to Kyle Simpson for the
+              <OutboundLink href="https://github.com/getify/You-Dont-Know-JS">
+                YDKJS
+              </OutboundLink>{" "}
+              series)
+            </p>
 
-            <p>I'm also a core contributor to many open source projects that I use regularly like
-              <OutboundLink href="https://bugzilla.mozilla.org/user_profile?user_id=614754">Firefox</OutboundLink>.
-              Recently I have been working in the domain of Artificial Intelligence and watching the development of
-              <OutboundLink href="https://js.tensorflow.org/">TFJS</OutboundLink> closely. </p>
-
+            <p>
+              I'm also a core contributor to many open source projects that I
+              use regularly like
+              <OutboundLink href="https://bugzilla.mozilla.org/user_profile?user_id=614754">
+                Firefox
+              </OutboundLink>
+              . Recently I have been working in the domain of Artificial
+              Intelligence and watching the development of
+              <OutboundLink href="https://js.tensorflow.org/">
+                TFJS
+              </OutboundLink>{" "}
+              closely.
+            </p>
           </div>
 
           <div className="content-block">
             {/*<p class="--reduced-margin-bottom">I work with following technologies</p>*/}
             <div className="icons-block">
               <div className="icon">
-                <FontAwesomeIcon icon={faJsSquare}/>
-                <FontAwesomeIcon icon={faPython}/>
-                <FontAwesomeIcon icon={faAws}/>
-                <FontAwesomeIcon icon={faDocker}/>
-                <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                <FontAwesomeIcon icon={faJsSquare} />
+                <FontAwesomeIcon icon={faPython} />
+                <FontAwesomeIcon icon={faAws} />
+                <FontAwesomeIcon icon={faDocker} />
+                <svg
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 448 512"
+                >
                   <g>
-                    <path fill="currentColor"
-                          d="M264 112H24a24 24 0 0 0 0 48h240a24 24 0 0 0 0-48zm352 0H376a24 24 0 0 0 0 48h240a24 24 0 0 0 0-48z">
-                    </path>
-                    <path fill="currentColor"
-                          d="M472 192v45.65l-96.83-29.05a191.93 191.93 0 0 0-110.34 0L168 237.65V192h-48v64.05a32 32 0 0 0 32 32h45.41a179 179 0 0 0-53.33 110.24 16.14 16.14 0 0 0 16 17.71h16.26c8.33 0 14.75-6.58 15.68-14.87a130 130 0 0 1 53.66-91.38l32.93 32.93a32 32 0 0 0 22.62 9.37h37.49a32 32 0 0 0 22.63-9.37l32.93-32.93A130 130 0 0 1 448 401.13c.93 8.29 7.35 14.85 15.68 14.87h16.22a16.13 16.13 0 0 0 16-17.71 178.94 178.94 0 0 0-53.32-110.24H488a32 32 0 0 0 32-32V192zM144 96a23.68 23.68 0 0 0-22.23 16h44.5A23.72 23.72 0 0 0 144 96zm352 0a23.68 23.68 0 0 0-22.23 16h44.5A23.72 23.72 0 0 0 496 96z">
-                    </path>
+                    <path
+                      fill="currentColor"
+                      d="M264 112H24a24 24 0 0 0 0 48h240a24 24 0 0 0 0-48zm352 0H376a24 24 0 0 0 0 48h240a24 24 0 0 0 0-48z"
+                    ></path>
+                    <path
+                      fill="currentColor"
+                      d="M472 192v45.65l-96.83-29.05a191.93 191.93 0 0 0-110.34 0L168 237.65V192h-48v64.05a32 32 0 0 0 32 32h45.41a179 179 0 0 0-53.33 110.24 16.14 16.14 0 0 0 16 17.71h16.26c8.33 0 14.75-6.58 15.68-14.87a130 130 0 0 1 53.66-91.38l32.93 32.93a32 32 0 0 0 22.62 9.37h37.49a32 32 0 0 0 22.63-9.37l32.93-32.93A130 130 0 0 1 448 401.13c.93 8.29 7.35 14.85 15.68 14.87h16.22a16.13 16.13 0 0 0 16-17.71 178.94 178.94 0 0 0-53.32-110.24H488a32 32 0 0 0 32-32V192zM144 96a23.68 23.68 0 0 0-22.23 16h44.5A23.72 23.72 0 0 0 144 96zm352 0a23.68 23.68 0 0 0-22.23 16h44.5A23.72 23.72 0 0 0 496 96z"
+                    ></path>
                   </g>
                 </svg>
               </div>
@@ -177,23 +262,27 @@ const IndexBody = ({posts}) => {
             {/*      </div>*/}
           </div>
 
-          <span style={{"text-align": "center", "display": "block"}}>
-            <br/>
-            <span><FontAwesomeIcon icon={faVideo}/></span> &nbsp;
-            <span>Sounds Interesting?
-              <OutboundLink href="https://calendly.com/mayank-raj/catch-up"> Hop on a call with me </OutboundLink>
+          <span style={{ "text-align": "center", display: "block" }}>
+            <br />
+            <span>
+              <FontAwesomeIcon icon={faVideo} />
+            </span>
+            &nbsp;
+            <span>
+              Sounds Interesting?
+              <OutboundLink href="https://calendly.com/mayank-raj/catch-up">
+                Hop on a call with me
+              </OutboundLink>
             </span>
           </span>
 
-          <BlogListing posts={posts} is_compact={true} show_bl> </BlogListing>
+          <BlogListing posts={posts} is_compact={true} show_bl></BlogListing>
           {/*<IndexBlogPreview posts={posts}> </IndexBlogPreview>*/}
         </div>
       </div>
     </div>
-
   );
 };
-
 
 // const IndexBlogPreview = ({posts}) => {
 //   return (
@@ -273,7 +362,6 @@ const IndexBody = ({posts}) => {
 //   );
 // };
 
-
 // TODO: Convert the blog into actual blogs
 class IndexPage extends React.Component {
   render() {
@@ -288,7 +376,6 @@ class IndexPage extends React.Component {
 
         <PageFooter> </PageFooter>
       </section>
-
     );
   }
 }
@@ -302,7 +389,10 @@ export const pageQuery = graphql`
         title
       }
     }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }, limit: 20) {
+    allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+      limit: 20
+    ) {
       edges {
         node {
           excerpt
